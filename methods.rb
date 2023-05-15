e09dbabe318c94d3105313cec613f00f4d4872c9
Olaf Mihari <olafnjenga@gmail.com>
2023-05-15 16:56:27 +0300

    completed assignment

--- methods.rb
+++ methods.rb
@@ -1,1 +1,44 @@
 # Your code here!
+def greet_programmer
+    puts "Hello, programmer!"
+end  
+
+
+
+def greet(name)
+    puts "Hello, #{name}!"
+end  
+ greet ("Naureen")  
+
+ 
+def greet_with_default(name = "Programmer")
+    puts "Hello, #{name}!"
+end 
+ 
+greet_with_default()
+greet_with_default("Sunny") 
+
+
+#    num
+
+def add(num1, num2)
+    num1 + num2
+end
+
+sum= add(1,2)
+puts sum
+
+def halve(number)
+    return nil unless number.is_a? Numeric
+
+    number / 2
+end
+
+result = halve(4)
+puts result
+
+result = halve(2)
+puts result
+
+
+
